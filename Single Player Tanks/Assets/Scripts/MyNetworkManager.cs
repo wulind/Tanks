de0c8d77cb6338ace397349c4491e5a102b1c615
Linda Wu <linda.wu723@gmail.com>
2019-03-11 02:56:48 +0000

    Adding server to Single Player Game

---
+++ Single Player Tanks/Assets/Scripts/MyNetworkManager.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.Networking;
+
+namespace TanksMP
+{
+    public class MyNetworkManager : NetworkManager {
+        public override void OnClientDisconnect(NetworkConnection conn)
+        {
+            base.OnClientDisconnect(conn);
+            NetworkedPlayer[] players = Resources.FindObjectsOfTypeAll<NetworkedPlayer>();
+            foreach (NetworkedPlayer player in players)
+            {
+                player.KillPlayer();
+            }
+        }
+
+        public override void OnServerDisconnect(NetworkConnection conn)
+        {
+            Application.Quit();//abandon game
+            base.OnServerDisconnect(conn);
+        }
+    }
+}
